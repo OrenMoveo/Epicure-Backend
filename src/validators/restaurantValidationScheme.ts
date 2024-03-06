32a6fe85a6268250d9163f52ba85e8e1b4e8b660
Oren Partovi <orenp@moveo.co.il>
2024-03-06 10:54:54 +0200

    openNow works

--- src/validators/restaurantValidationScheme.ts
+++ src/validators/restaurantValidationScheme.ts
@@ -1,6 +1,6 @@
 import Joi from "joi";
 import { IRestaurant } from "../models/restaurant";
-import { chefObjectIdSchema, dishObjectIdSchema } from './validate';
+import { chefObjectIdSchema, dishObjectIdSchema } from "./validate";
 
 const priceRangeSchema = Joi.array()
   .items(Joi.number())
@@ -18,24 +18,38 @@ export const createRestaurantValidationSchema = Joi.object<IRestaurant>({
   name: Joi.string().required().max(30),
   chef: chefObjectIdSchema.required(),
   pictureUrl: Joi.string().required(),
-  openNow: Joi.boolean().required(),
   rating: Joi.number().min(1).max(5).required(),
   mostPopular: Joi.boolean().required(),
   newRestaurant: Joi.boolean().required(),
   restaurantDishes: Joi.array().items(dishObjectIdSchema),
   priceRange: priceRangeSchema.required(),
   isPopular: Joi.boolean().required(),
+  openingHours: Joi.object({
+    openTime: Joi.string()
+      .regex(/^([01]\d|2[0-3]):[0-5]\d$/)
+      .required(),
+    closeTime: Joi.string()
+      .regex(/^([01]\d|2[0-3]):[0-5]\d$/)
+      .required(),
+  }).required(),
 });
 
 export const updateRestaurantValidationSchema = Joi.object<IRestaurant>({
   name: Joi.string().optional().max(30),
   chef: chefObjectIdSchema.optional(),
   pictureUrl: Joi.string().optional(),
-  openNow: Joi.boolean().optional(),
   rating: Joi.number().optional(),
   mostPopular: Joi.boolean().optional(),
   newRestaurant: Joi.boolean().optional(),
   restaurantDishes: Joi.array().items(dishObjectIdSchema).optional(),
   priceRange: priceRangeSchema.optional(),
   isPopular: Joi.boolean().optional(),
+  openingHours: Joi.object({
+    openTime: Joi.string()
+      .regex(/^([01]\d|2[0-3]):[0-5]\d$/)
+      .optional(),
+    closeTime: Joi.string()
+      .regex(/^([01]\d|2[0-3]):[0-5]\d$/)
+      .optional(),
+  }).optional(),
 });
